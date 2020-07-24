893d7b21dee9c2463862f674b884fbccd0225f34
Ethan Halsall <ethanhalsall11@augustana.edu>
2020-07-24 01:26:19 -0500

    chore: add annotation docs to CueFileGenerator

--- test/IcecastMetadataRecorder.test.js
+++ test/IcecastMetadataRecorder.test.js
@@ -1,9 +1,9 @@
 const IcecastMetadataRecorder = require("../src/IcecastMetadataRecorder");
 
 const testStream = new IcecastMetadataRecorder({
-    fileName: "isics-all.mp3",
-    streamTitle: "ISICS All",
-    streamEndpoint: "https://dsmrad.io/stream/isics-all",
-  });
-  
+  fileName: "isics-all.mp3",
+  streamTitle: "ISICS All",
+  streamEndpoint: "https://dsmrad.io/stream/isics-all",
+});
+
 testStream.record();
