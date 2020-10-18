6a4b02ffcd8a5f23f73fee10b6285fd58d0cb0bd
Ethan Halsall <ethanhalsall11@augustana.edu>
2020-10-18 15:40:36 -0500

    Updates

---
+++ precache-manifest.dc777863b8c5421ba39dc3ad77b75284.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff300811e4f920c7eac3d153ad75b3c8",
+    "url": "/icecast-metadata-js/index.html"
+  },
+  {
+    "revision": "e2f774776929be416aca",
+    "url": "/icecast-metadata-js/static/css/main.27ef7586.chunk.css"
+  },
+  {
+    "revision": "edef96e03b87c43c2aa1",
+    "url": "/icecast-metadata-js/static/js/2.a083189d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/icecast-metadata-js/static/js/2.a083189d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2f774776929be416aca",
+    "url": "/icecast-metadata-js/static/js/main.f27e775e.chunk.js"
+  },
+  {
+    "revision": "028449cb7ceb94d3b7d2",
+    "url": "/icecast-metadata-js/static/js/runtime-main.e917606f.js"
+  },
+  {
+    "revision": "b0b65543a67846c7181811d28ba34acf",
+    "url": "/icecast-metadata-js/static/media/pause.b0b65543.svg"
+  },
+  {
+    "revision": "0187bd2f4411fd60c98ea463bc435135",
+    "url": "/icecast-metadata-js/static/media/play.0187bd2f.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/icecast-metadata-js/precache-manifest.dc777863b8c5421ba39dc3ad77b75284.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/icecast-metadata-js/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
