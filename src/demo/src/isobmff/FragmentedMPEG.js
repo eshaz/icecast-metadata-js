c3f343be3f659aa4529626af7e7ce6f7b48a1cd7
Ethan Halsall <ethanhalsall11@augustana.edu>
2020-11-01 17:58:21 -0600

    fix: bit reservoir

--- src/demo/src/isobmff/FragmentedMPEG.js
+++ src/demo/src/isobmff/FragmentedMPEG.js
@@ -5,6 +5,7 @@ import FragmentedISOBMFFBuilder from "./FragmentedISOBMFFBuilder";
 export default class FragmentedMPEG {
   constructor() {
     this._frames = [];
+    this._totalLength = 0;
     this._partialFrame = new Uint8Array(0);
   }
 
@@ -24,17 +25,21 @@ export default class FragmentedMPEG {
     while (frame && frame.isComplete) {
       this._frames.push(frame.data);
       offset += frame.data.length;
+      this._totalLength += frame.data.length;
       frame = MP3Parser.readFrame(newBuffer, offset).frame;
     }
 
     this._partialFrame = newBuffer.subarray(offset);
 
-    if (this._frames.length > 1) {
+    // Allow for +-511 bytes for bit reservoir
+    if (this._frames.length > 1 && this._totalLength > 1022) {
+      //console.log(this._frames)
       const fragments = FragmentedISOBMFFBuilder.wrapMp3InMovieFragment(
         this._frames
       );
       //console.log(this._partialFrame.length, this._frames.length);
       this._frames = [];
+      this._totalLength = 0;
       return fragments;
     }
     return new Uint8Array(0);
