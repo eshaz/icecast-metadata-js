253316c9ddd7263d1d1cffde6fd8fb8143bd1eb7
Ethan Halsall <ethanhalsall11@augustana.edu>
2021-03-14 14:49:52 -0500

    feat: split out ogg and icy metadata

--- src/icecast-metadata-player/src/MetadataGrabber.js
+++ src/icecast-metadata-player/src/MetadataGrabber.js
@@ -16,17 +16,19 @@ export default class MetadataGrabber {
 
     this._icestatsEndpoint =
       options.icestatsEndpoint || `${serverPath}/status-json.xsl`;
+    this._statsEndpoint = options.statsEndpoint || `${serverPath}/stats`;
     this._sevenHtmlEndpoint =
       options.sevenHtmlEndpoint || `${serverPath}/7.html`;
 
     this._statsMethods = options.statsMethods || [
       "icestats",
-      "shoutcast",
+      "stats",
+      "sevenHtml",
       "icy",
       "ogg",
     ];
     this._interval = options.interval || 20000;
-    this._onStats = options.onStats || noOp;
+    this._onStats = options.onStats || console.log;
 
     this._state = STOPPED;
   }
@@ -39,17 +41,11 @@ export default class MetadataGrabber {
     if (this._state === STOPPED) {
       this._state = RUNNING;
 
-      this.getStats().then((stats) => {
-        console.log(stats);
-        this._onStats(stats);
-      });
+      this.getMetadata();
 
       this._intervalId = setInterval(() => {
         console.log("interval");
-        this.getStats().then((stats) => {
-          console.log(stats);
-          this._onStats(stats);
-        });
+        this.getMetadata();
       }, this._interval);
     }
   }
@@ -59,15 +55,17 @@ export default class MetadataGrabber {
       this._state = STOPPED;
 
       clearInterval(this._intervalId);
-      this._streamController.abort();
+      this._icyController.abort();
+      this._oggController.abort();
       this._icestatsController.abort();
       this._sevenHtmlController.abort();
     }
   }
 
-  async getStats() {
+  async getMetadata() {
     if (this._state === RUNNING) {
-      this._streamController = new AbortController();
+      this._icyController = new AbortController();
+      this._oggController = new AbortController();
       this._icestatsController = new AbortController();
       this._sevenHtmlController = new AbortController();
 
@@ -76,32 +74,30 @@ export default class MetadataGrabber {
       promises.push(this.getIcestats());
       //if (this._statsMethods.includes("sevenHtml"))
       promises.push(this.getSevenHtml());
-      if (
-        this._statsMethods.includes("icy") ||
-        this._statsMethods.includes("ogg")
-      )
-        promises.push(this.getStreamMetadata());
+      //if (this._statsMethods.includes("stats"))
+      //promises.push(this.getStats());
+      //if (this._statsMethods.includes("icy"))
+      promises.push(this.getIcyMetadata());
+      //if (this._statsMethods.includes("ogg"))
+      promises.push(this.getOggMetadata());
 
       const stats = await Promise.all(promises).then((stats) =>
         stats.reduce((acc, stat) => ({ ...acc, ...stat }), {})
       );
       this._state = RUNNING;
+      this._onStats(stats);
       return stats;
     }
   }
 
   async getIcestats() {
-    this._state = FETCHING;
-
-    return fetch(this._icestatsEndpoint, {
-      signal: this._icestatsController.signal,
-    })
-      .then((res) => res.json())
-      .catch((e) => {
-        if (e.name !== "AbortError") {
-          throw e;
-        }
-      });
+    return this._fetch({
+      endpoint: this._icestatsEndpoint,
+      controller: this._icestatsController,
+      mapper: (res) => res.json(),
+    }).finally(() => {
+      this._icestatsController = new AbortController();
+    });
   }
 
   /*
@@ -115,14 +111,11 @@ export default class MetadataGrabber {
   // http://wiki.winamp.com/wiki/SHOUTcast_DNAS_Server_2_XML_Reponses#Equivalent_of_7.html
   // CURRENTLISTENERS STREAMSTATUS PEAKLISTENERS MAXLISTENERS UNIQUELISTENERS BITRATE SONGTITLE
   async getSevenHtml() {
-    this._state = FETCHING;
-
-    return fetch(this._sevenHtmlEndpoint, {
-      signal: this._sevenHtmlController.signal,
-    })
-      .then((res) => res.text())
-      .then((shoutcastStats) => ({
-        sevenHtml: shoutcastStats.match(/(.*?)<\/body>/gi).map((s) => {
+    return this._fetch({
+      endpoint: this._sevenHtmlEndpoint,
+      controller: this._sevenHtmlController,
+      mapper: async (res) => ({
+        sevenHtml: (await res.text()).match(/(.*?)<\/body>/gi).map((s) => {
           const stats = s
             .match(/(<body>|,)(?<stats>.*)<\/body>/i)
             .groups.stats.split(",");
@@ -145,43 +138,70 @@ export default class MetadataGrabber {
                 bitrate: parseInt(stats[3]),
               };
         }),
-      }))
-      .catch((e) => {
-        if (e.name !== "AbortError") {
-          throw e;
-        }
-      });
+      }),
+    }).finally(() => {
+      this._sevenHtmlController = new AbortController();
+    });
   }
 
-  async getStreamMetadata() {
-    this._state = FETCHING;
-
-    return fetch(this._streamEndpoint, {
-      headers: this._statsMethods.includes("icy") ? { "Icy-MetaData": 1 } : {},
-      signal: this._streamController.signal,
-    })
-      .then(async (res) => {
-        let meta;
-        await new Promise((resolve) => {
+  async getIcyMetadata() {
+    return this._fetch({
+      endpoint: this._streamEndpoint,
+      controller: this._icyController,
+      headers: { "Icy-MetaData": 1 },
+      mapper: async (res) =>
+        new Promise((resolve) => {
           new IcecastReadableStream(res, {
             onMetadata: ({ metadata }) => {
-              if (!meta) {
-                this._streamController.abort();
-                meta = metadata;
-                resolve();
-              }
+              this._icyController.abort();
+              resolve({ icy: metadata });
             },
-            metadataTypes: this._statsMethods,
+            onMetadataFailed: () => {
+              this._icyController.abort();
+              resolve({ icy: {} });
+            },
+            metadataTypes: ["icy"],
           }).startReading();
-        });
+        }),
+    }).finally(() => {
+      this._icyController = new AbortController();
+    });
+  }
 
-        this._streamController = new AbortController();
+  async getOggMetadata() {
+    return this._fetch({
+      endpoint: this._streamEndpoint,
+      controller: this._oggController,
+      mapper: async (res) =>
+        new Promise((resolve) => {
+          new IcecastReadableStream(res, {
+            onMetadata: ({ metadata }) => {
+              this._oggController.abort();
+              resolve({ ogg: metadata });
+            },
+            onMetadataFailed: () => {
+              this._oggController.abort();
+              resolve({ ogg: {} });
+            },
+            metadataTypes: ["ogg"],
+          }).startReading();
+        }),
+    }).finally(() => {
+      this._oggController = new AbortController();
+    });
+  }
 
-        return { icy: meta };
-      })
+  async _fetch({ endpoint, controller, mapper, headers = {} }) {
+    this._state = FETCHING;
+
+    return fetch(endpoint, {
+      headers,
+      signal: controller.signal,
+    })
+      .then(mapper)
       .catch((e) => {
         if (e.name !== "AbortError") {
-          throw e;
+          console.warn(`Failed to fetch ${endpoint}`, e);
         }
       });
   }
