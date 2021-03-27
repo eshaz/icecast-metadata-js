60890939b61e96fd94c7774c3d1c892881bfd55b
Ethan Halsall <ethanhalsall11@augustana.edu>
2021-03-27 12:19:33 -0500

    feat: update demo pages

--- src/demo/src/Player/Player.jsx
+++ src/demo/src/Player/Player.jsx
@@ -1,7 +1,5 @@
 import React, { useLayoutEffect, useState } from "react";
 import AudioSpectrum from "react-audio-spectrum";
-import { ReactComponent as Play } from "./play.svg";
-import { ReactComponent as Pause } from "./pause.svg";
 import styles from "./Player.module.css";
 
 const VISIT_STATION = "Visit this station at ";
@@ -49,9 +47,23 @@ const Player = ({ station, playing, toggle, audioElement, metadata }) => {
         />
       </div>
       <button disabled={!station} className={styles.button} onClick={toggle}>
-        {playing ? <Pause /> : <Play />}
+        {playing ? (
+          <svg className={styles.playPause} viewBox="0 0 450 525">
+            <path
+              fill="#808080"
+              d="M144 479H48c-26.5 0-48-21.5-48-48V79c0-26.5 21.5-48 48-48h96c26.5 0 48 21.5 48 48v352c0 26.5-21.5 48-48 48zm304-48V79c0-26.5-21.5-48-48-48h-96c-26.5 0-48 21.5-48 48v352c0 26.5 21.5 48 48 48h96c26.5 0 48-21.5 48-48z"
+            />
+          </svg>
+        ) : (
+          <svg className={styles.playPause} viewBox="0 0 450 525">
+            <path
+              fill="#808080"
+              d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z"
+            />
+          </svg>
+        )}
       </button>
-      <div>
+      <div className={styles.playerText}>
         <p className={styles.metadata}>
           {typeof metadata === "object"
             ? metadata.StreamTitle ||
