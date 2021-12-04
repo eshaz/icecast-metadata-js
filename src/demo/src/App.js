1b477bc767e86c37c7e48d714d4a2924398aae61
Ethan Halsall <ethanhalsall11@augustana.edu>
2021-12-04 12:03:15 -0600

    feat: add chromecast demo page

--- src/demo/src/App.js
+++ src/demo/src/App.js
@@ -31,13 +31,66 @@ const App = () => {
 
   const analyzer = useRef();
   const [audioMotion, setAudioMotion] = useState();
+  const [castSession, setCastSession] = useState();
+
+  const namespace = "urn:x-cast:icecast-metadata-js-demo";
+
+  const sendCastMessage = useCallback(
+    (msg) => {
+      console.log("sending", msg);
+      castSession?.sendMessage(
+        namespace,
+        msg,
+        () => console.log("Message sent: ", msg),
+        (...args) => console.log("error", ...args)
+      );
+    },
+    [castSession]
+  );
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "//www.gstatic.com/cv/js/sender/v1/cast_sender.js";
+    document.body.appendChild(script);
+
+    window.__onGCastApiAvailable = (loaded, error) => {
+      console.log("cast", loaded, error);
+      if (loaded) {
+        const sessionRequest = new window.chrome.cast.SessionRequest(
+          "E3C20492"
+        );
+
+        const apiConfig = new window.chrome.cast.ApiConfig(
+          sessionRequest,
+          (session) => {
+            console.log("New session ID:" + session.sessionId);
+            setCastSession(session);
+          },
+          (...e) => console.log("error", ...e)
+        );
+
+        window.chrome.cast.initialize(
+          apiConfig,
+          (...args) => console.log("success", ...args),
+          (...args) => console.log("error", ...args)
+        );
+      }
+    };
+  }, []);
 
   const changeStation = useCallback(
     async (station) => {
       if (icecast) {
         await icecast.stop();
+        sendCastMessage({ command: "stop" });
         icecast.detachAudioElement();
       }
+
+      sendCastMessage({
+        command: "change station",
+        enableCodecUpdate: true,
+        ...station,
+      });
 
       const player = new IcecastMetadataPlayer(station.endpoint, {
         onMetadata: (meta) => {
@@ -76,16 +129,27 @@ const App = () => {
       });
 
       player.play();
+      sendCastMessage({ command: "play" });
 
       setIcecast(player);
       setStation(station);
     },
-    [icecast, audioElement]
+    [icecast, audioElement, sendCastMessage]
   );
 
+  const play = useCallback(() => {
+    icecast.play();
+    sendCastMessage({ command: "play" });
+  }, [icecast, sendCastMessage]);
+
+  const stop = useCallback(() => {
+    icecast.stop();
+    sendCastMessage({ command: "stop" });
+  }, [icecast, sendCastMessage]);
+
   const toggle = useCallback(() => {
-    playing ? icecast.stop() : icecast.play();
-  }, [icecast, playing]);
+    playing ? stop() : play();
+  }, [playing, stop, play]);
 
   useEffect(() => {
     if (window.AudioContext) {
