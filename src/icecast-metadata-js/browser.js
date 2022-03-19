87d71b3be2344a75d40975392980afb579f99a0f
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-19 12:36:47 +0800

    Delete src directory

--- src/icecast-metadata-js/browser.js
+++
@@ -1,5 +0,0 @@
-import IcecastMetadataQueue from "./src/IcecastMetadataQueue";
-import IcecastMetadataReader from "./src/IcecastMetadataReader";
-import IcecastReadableStream from "./src/IcecastReadableStream";
-
-export { IcecastMetadataQueue, IcecastMetadataReader, IcecastReadableStream };
