ce10afa427935f27fbdffff08c0cc04283cc83b2
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-19 12:36:46 +0800

    Delete src directory

--- src/icecast-metadata-js/index.js
+++
@@ -1,9 +0,0 @@
-const IcecastMetadataQueue = require("./src/IcecastMetadataQueue");
-const IcecastMetadataReader = require("./src/IcecastMetadataReader");
-const IcecastMetadataStream = require("./src/IcecastMetadataStream");
-
-module.exports = {
-  IcecastMetadataQueue,
-  IcecastMetadataReader,
-  IcecastMetadataStream,
-};
