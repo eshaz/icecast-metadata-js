103efce4ca10a976c5f80a5d7d5872d0c3fe17f2
Ethan Halsall <ethanhalsall11@augustana.edu>
2024-05-14 20:16:59 -0500

    feat(demo): tweak vintage page

--- src/demo/build-vintage.mjs
+++ src/demo/build-vintage.mjs
@@ -10,28 +10,43 @@ const buildPlaylist = (endpoint) =>
     .replaceAll("https", "http");
 
 const getColor = (codec) => {
-    if (codec.match(/mp3/i)) return "yellow";
     if (codec.match(/opus/i)) return "magenta";
     if (codec.match(/vorbis/i)) return "green";
     if (codec.match(/aac/i)) return "red";
     if (codec.match(/flac/i)) return "blue";
+    if (codec.match(/mp3/i)) return "yellow";
     return "yellow";
 }
 
 const getStationRow = (acc, station) => {
+    const maxCharacters = 25;
+    let totalCharacters = 0;
     return acc + `
         <tr>
           <td style="color: lime">${station.name}</td>
           <td>${station.description}</td>
-          <td>${station.endpoints.map(
-            (endpoint) => {
+          <td>${station.endpoints.reduce(
+            (acc, endpoint, idx) => {
                 const playlist = buildPlaylist(endpoint.endpointHttp || endpoint.endpoint);
                 const playlistName = `${station.name.replace("/", "").replace("'", "")} ${endpoint.codec ?? ""}.m3u`;
                 fs.writeFileSync(`public/vintage/${playlistName}`, playlist, {encoding: 'binary'});
 
-                return `<a download href="${encodeURI(playlistName)}"><b style="color: ${getColor(endpoint.codec)}">${endpoint.codec}</b></a>`
-            })
-            .join("|")
+                totalCharacters += endpoint.codec.length;
+
+                let separator = "";
+                if (idx !== station.endpoints.length - 1) {
+                  separator = "|";
+                  totalCharacters++;
+                }
+
+                let br = "";
+                if (totalCharacters > maxCharacters) {
+                  br = "<br>";
+                  totalCharacters = endpoint.codec.length;
+                }
+
+                return acc + `${br}<a style="color: ${getColor(endpoint.codec)};" download href="${encodeURI(playlistName)}">${endpoint.codec}</a>${separator}`
+            }, "")
           }
           </td>
         </tr>`
@@ -47,8 +62,9 @@ const html = `
       name="description"
       content="Vintage Streaming"
     />
-    <link href="RIBBONS.BMP" rel="stylesheet"/>
     <link href="title.gif" rel="stylesheet"/>
+    <link href="RIBBONS.BMP" rel="stylesheet"/>
+    <link href="MARBLE.BMP" rel="stylesheet"/>
     <style>
       body {
         font: 10px monospace;
@@ -66,17 +82,13 @@ const html = `
     <div style="text-align: center;" >
       <img src="title.gif" alt="Internet Radio for vintage computers!"/>
     </div>
-    <table style="margin-left: auto; margin-right: auto;" border="1">
+    <div style="border: black 1px solid; max-width: 730px; margin-left: auto; margin-right: auto; background-image: url('MARBLE.BMP'); padding: 5px;">
+    <table style="margin: 0;" border="1">
       <colgroup>
-          <col width="200px" />
-          <col width="350px" />
-          <col width="50px" />
+          <col width="180px" />
+          <col width="410px" />
+          <col width="150px" />
       </colgroup>
-      <tr>
-        <td style="text-align: center;" colspan="3">
-          Get Winamp <a href="http://ftp.zx.net.nz/pub/software/Win32/Media-Player/WinAmp/winamp524_full_emusic-7plus.exe">here (version 5.24)</a> and an Opus decoder <a href="https://github.com/RamonUnch/in_opus/releases">here</a>.
-        </td>
-      </tr>
       <tr>
         <th>Station</th>
         <th>About</th>
@@ -86,8 +98,22 @@ const html = `
       ${stations
         .filter((station) => !station.name.match(/multichannel/i))
         .reduce(getStationRow, "")}
+      <tr>
+        <td colspan="3">
+          <div>
+            &nbsp;
+          </div>
+          <div style="font-size: 14px; text-align: center;"> 
+            Get Winamp <a href="http://ftp.zx.net.nz/pub/software/Win32/Media-Player/WinAmp/winamp524_full_emusic-7plus.exe">here (version 5.24)</a> and an Opus decoder <a href="https://github.com/RamonUnch/in_opus/releases">here</a>.
+          </div>
+          <div style="text-align: right;">
+            (c) 2024 <a style="color: white;" href="https://github.com/eshaz">Ethan Halsall</a>
+          </div>
+        </td>
+      </tr>
       </tbody>
     </table>
+    </div
   </body>
 </html>
 `;
